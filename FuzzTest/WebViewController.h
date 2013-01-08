676513a0dc58ef88150dfc16a5fb160616fb8aec
Alex Basson <alexbasson@gmail.com>
2013-01-08 12:02:12 -0500

    Added WebViewController, laid it out in storyboard.

---
+++ FuzzTest/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  FuzzTest
+//
+//  Created by Alex on 1/8/13.
+//  Copyright (c) 2013 Alex Basson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
